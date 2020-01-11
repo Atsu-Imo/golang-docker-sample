becd3072059b3845944519ba99c03696cfba781d
Atsu-Imo <runo1go@gmail.com>
2020-01-12 01:43:27 +0900

    delete test data

--- database/init/1_create_table.sql
+++
@@ -1,8 +0,0 @@
-create table users
-(
-  id serial primary key,
-  username varchar(256) unique not null,
-  created_at date,
-  updated_at date,
-  deleted_at date
-);
--- database/init/2_insert_table.sql
+++
@@ -1,3 +0,0 @@
-insert into users (username) values ('Alice');
-insert into users (username) values ('Bob');
-insert into users (username) values ('Carol');
