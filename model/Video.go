cd21cf752d9b1e944ca42915479eacb479e38dc7
Atsu-Imo <runo1go@gmail.com>
2020-01-13 22:21:27 +0900

    model側にfind処理を移動（設計としてこれでよいかは要検討...） refs #2

--- model/Video.go
+++ model/Video.go
@@ -16,3 +16,15 @@ type Video struct {
 	Length      string
 	PublishedAt time.Time
 }
+
+// Repository DBとの接続を管理
+type Repository struct {
+	DB *gorm.DB
+}
+
+// FindAll すべてのデータを返す
+func (p *Repository) FindAll() []Video {
+	var videos []Video
+	p.DB.Find(&videos)
+	return videos
+}
