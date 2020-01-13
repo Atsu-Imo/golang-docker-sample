57b5e8617212123553046f6c3b53d60ea3d903de
Atsu-Imo <runo1go@gmail.com>
2020-01-13 16:38:24 +0900

    DB接続情報を.envファイルに分離

--- api/channel_handlers.go
+++ api/channel_handlers.go
@@ -1,18 +1,26 @@
 package api
 
 import (
+	"os"
 	"net/http"
 	"encoding/json"
 
 	"github.com/Atsu-Imo/golang-docker-sample/model"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
 
+	//postgres
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+//GetChannels すべてのチャンネル
 func GetChannels(c echo.Context) error {
-	db, err := gorm.Open("postgres", "host=postgres port=5432 user=postgres password=password dbname=test_db sslmode=disable")
+	err := godotenv.Load()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	db, err := gorm.Open("postgres", os.Getenv("DB"))
 	if err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
